6518b5b204a37adafc014469adc34c0c79ac369e
Josh Austin <83935880+joshaustintech@users.noreply.github.com>
2024-09-06 00:46:06 -0500

    starting star particle system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 use raylib::prelude::*;
 
+enum Size {
+    SMALL, MEDIUM, LARGE
+}
+struct Circle {
+    position: Vector2,
+    size: Size,
+    is_out_of_view: bool
+}
+/*
+impl Circle {
+
+    fn new() -> Circle {
+        return Circle {
+            position: Vector2 {
+                x: 12.0,
+                y: 12.0
+            },
+            size: Size::MEDIUM,
+            is_out_of_view: false
+        };
+    }
+
+    fn update(&mut self) {
+        let speed = match self.size {
+            Size::SMALL  => 2,
+            Size::MEDIUM => 4,
+            Size::LARGE  => 8
+        };
+    }
+}
+*/
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(640, 480)
         .title("Hello, World")
         .build();
+
+//    let particles: Vec<Circle> = vec!();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
