8a2e7bbdf2872853cb7489558792f8d26cc3446d
Josh Austin <83935880+joshaustintech@users.noreply.github.com>
2024-09-07 20:09:52 -0500

    Working star particle system

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Vertical space shooter inspired by @ianthetechie's game Bit Blaster (no longer a
 # usage
 ## prerequisites
 - Rust 1.80.1 or newer
+- Python 3
 - Raylib prerequisites (CMake, etc.)
 - Emscripten
 - Add wasm32 emscripten compile target
@@ -14,8 +15,7 @@ $ rustup target add wasm32-unknown-emscripten
 ```sh
 $ cargo run
 ```
-## macOS compile instructions for web
+## compile instructions for web
 ```sh
-$ export EMCC_CFLAGS="-O3 -sUSE_GLFW=3 -sGL_ENABLE_GET_PROC_ADDRESS -sWASM=1 -sALLOW_MEMORY_GROWTH=1 -sWASM_MEM_MAX=512MB -sTOTAL_MEMORY=512MB -sABORTING_MALLOC=0 -sASYNCIFY -sFORCE_FILESYSTEM=1 -sASSERTIONS=1 -sERROR_ON_UNDEFINED_SYMBOLS=0 -sEXPORTED_RUNTIME_METHODS=ccallcwrap"
-$ cargo build --target wasm32-unknown-emscripten
+$ make debug
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,129 @@
 use raylib::prelude::*;
 
+#[derive(Clone)]
 enum Size {
     SMALL, MEDIUM, LARGE
 }
+
+#[derive(Clone)]
 struct Circle {
     position: Vector2,
     size: Size,
     is_out_of_view: bool
 }
-/*
+
 impl Circle {
 
-    fn new() -> Circle {
+    fn new(rl: &RaylibHandle, is_random_height: bool) -> Circle {
+        let random_horizontal: i32 = rl.get_random_value(0..rl.get_screen_width()-1);
+        let vertical: i32 = match is_random_height {
+            true  => rl.get_random_value(0..rl.get_screen_height()-1),
+            false => 0
+        };
         return Circle {
             position: Vector2 {
-                x: 12.0,
-                y: 12.0
+                x: (random_horizontal + 16) as f32 - 8.0,
+                y: vertical as f32 - 8.0
             },
-            size: Size::MEDIUM,
+            size: match rl.get_random_value(0..2) {
+                0 => Size::SMALL,
+                1 => Size::MEDIUM,
+                _ => Size::LARGE
+            },
             is_out_of_view: false
         };
     }
 
-    fn update(&mut self) {
-        let speed = match self.size {
-            Size::SMALL  => 2,
-            Size::MEDIUM => 4,
-            Size::LARGE  => 8
-        };
+    fn update(&mut self, rl: &RaylibHandle) {
+        self.position.y = self.position.y + ((rl.get_screen_height() / 60 / 4) as f32 * match self.size {
+            Size::SMALL  => 0.25,
+            Size::MEDIUM => 0.5,
+            Size::LARGE  => 1.0
+        });
+
+        if self.position.y >= rl.get_screen_height() as f32 + 8.0 {
+            self.is_out_of_view = true;
+        }
+
     }
+
 }
-*/
+
+struct Starfield {
+    stars: Vec<Circle>
+}
+
+impl Starfield {
+
+    fn new(rl: &RaylibHandle) -> Starfield {
+        let mut starfield: Starfield = Starfield { stars: vec!() };
+        for _ in 1..50 {
+            starfield.stars.push(Circle::new(rl, true));
+        }
+        return starfield;
+    }
+
+    fn update(&mut self, rl: &RaylibHandle) {
+
+        let mut new_starfield: Vec<Circle> = vec!();
+
+        let random: i32 = rl.get_random_value(1..7);
+        if random == 3 {
+            let new_star: Circle = Circle::new(rl, false);
+            new_starfield.push(new_star);
+        }
+
+        loop {
+            match self.stars.pop() {
+                Some(mut star) => {
+                    star.update(&rl);
+                    if !star.is_out_of_view {
+                        new_starfield.push(star);
+                    } else {
+                        println!("Removing star at {}, {}", star.position.x, star.position.y);
+                    }
+                },
+                None => break
+            }
+        }
+
+        self.stars = new_starfield;
+    }
+
+    fn draw(&mut self, d: &mut RaylibDrawHandle<'_>) {
+        for star in self.stars.clone() {
+            d.draw_circle(
+                star.position.x as i32,
+                star.position.y as i32,
+                match star.size {
+                    Size::SMALL  => 1.0,
+                    Size::MEDIUM => 1.5,
+                    Size::LARGE  => 2.0
+                },
+                Color::WHITE);
+        }
+    }
+
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Hello, World")
+        .size(800, 600)
+        .title("Space Arcade")
         .build();
 
-//    let particles: Vec<Circle> = vec!();
+    let mut starfield: Starfield = Starfield::new(&rl);
 
+    rl.set_target_fps(60);
+    //rl.set_window_max_size(800, 600);
     while !rl.window_should_close() {
+
+        starfield.update(&rl);
+
         let mut d = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        starfield.draw(&mut d);
+
+        d.clear_background(Color::BLACK);
     }
 }
